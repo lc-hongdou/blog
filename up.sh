a2a5a1d6644a0adeef50bc252f4cdbd12ae7227e
YOS-262V6SA0LGU <379535454@qq.com>
2023-04-11 16:12:51 +0800

    vault backup: 2023-04-11 16:12:51

---
+++ up.sh
@@ -0,0 +1,11 @@
+# deploy_hexo.sh
+cd /d/hexoblog
+pwd
+# 白底黑字效果
+echo -e "\033[47;30m>>>>>>>>>>>>>>>>>>>>hexo g<<<<<<<<<<<<<<<<<<<<\033[0m"
+hexo g
+echo -e "\033[47;30m>>>>>>>>>>>>>>>>>>>>hexo d<<<<<<<<<<<<<<<<<<<<\033[0m"
+hexo d
+sleep 5
+# 执行完毕不退出
+# exec /bin/bash
